586e42fa4941a1d97a2b80b1d83cba7a411db5ad
Elliot Wright <wright.elliot@gmail.com>
2014-05-24 22:45:17 +0100

    Removed uncessary % syntax from parameters.

--- src/Trident/Module/FrameworkModule/config/services.php
+++ src/Trident/Module/FrameworkModule/config/services.php
@@ -2,24 +2,24 @@
 
 return function($container) {
     // Parameters
-    $container['%controller_resolver.class%'] = 'Strident\\Component\\HttpKernel\\Controller\\ControllerResolver';
-    $container['%event_dispatcher.class%']    = 'Symfony\\Component\\EventDispatcher\\EventDispatcher';
-    $container['%http_kernel.class%']         = 'Symfony\\Component\\HttpKernel\\HttpKernel';
-    $container['%request_stack.class%']       = 'Symfony\\Component\\HttpFoundation\\RequestStack';
-    $container['%route_collection.class%']    = 'Symfony\\Component\\Routing\\RouteCollection';
-    $container['%listener.exception.class%']  = 'Symfony\\Component\\HttpKernel\\EventListener\\ExceptionListener';
-    $container['%listener.response.class%']   = 'Symfony\\Component\\HttpKernel\\EventListener\\ResponseListener';
-    $container['%listener.router.class%']     = 'Symfony\\Component\\HttpKernel\\EventListener\\RouterListener';
-    $container['%exception_controller%']      = 'Strident\\Module\\FrameworkModule\\Controller\\ExceptionController::exceptionAction';
+    $container['controller_resolver.class'] = 'Strident\\Component\\HttpKernel\\Controller\\ControllerResolver';
+    $container['event_dispatcher.class']    = 'Symfony\\Component\\EventDispatcher\\EventDispatcher';
+    $container['http_kernel.class']         = 'Symfony\\Component\\HttpKernel\\HttpKernel';
+    $container['request_stack.class']       = 'Symfony\\Component\\HttpFoundation\\RequestStack';
+    $container['route_collection.class']    = 'Symfony\\Component\\Routing\\RouteCollection';
+    $container['listener.exception.class']  = 'Symfony\\Component\\HttpKernel\\EventListener\\ExceptionListener';
+    $container['listener.response.class']   = 'Symfony\\Component\\HttpKernel\\EventListener\\ResponseListener';
+    $container['listener.router.class']     = 'Symfony\\Component\\HttpKernel\\EventListener\\RouterListener';
+    $container['exception_controller']      = 'Strident\\Module\\FrameworkModule\\Controller\\ExceptionController::exceptionAction';
 
 
     // Services
     $container->set('controller_resolver', function($c) {
-        return new $c['%controller_resolver.class%']($c);
+        return new $c['controller_resolver.class']($c);
     });
 
     $container->set('event_dispatcher', function($c) {
-        return new $c['%event_dispatcher.class%']();
+        return new $c['event_dispatcher.class']();
     });
 
     $container->set('http_kernel', function($c) {
@@ -27,15 +27,15 @@ return function($container) {
         $resolver     = $c['controller_resolver'];
         $requestStack = $c['request_stack'];
 
-        return new $c['%http_kernel.class%']($dispatcher, $resolver, $requestStack);
+        return new $c['http_kernel.class']($dispatcher, $resolver, $requestStack);
     });
 
     $container->set('listener.exception', function($c) {
-        return new $c['%listener.exception.class%']($c['%exception_controller%']);
+        return new $c['listener.exception.class']($c['exception_controller']);
     });
 
     $container->set('listener.response', function($c) {
-        return new $c['%listener.response.class%']('UTF-8');
+        return new $c['listener.response.class']('UTF-8');
     });
 
     $container->set('listener.router', function($c) {
@@ -43,15 +43,15 @@ return function($container) {
         $context = new Symfony\Component\Routing\RequestContext();
         $matcher = new Symfony\Component\Routing\Matcher\UrlMatcher($routes, $context);
 
-        return new $c['%listener.router.class%']($matcher);
+        return new $c['listener.router.class']($matcher);
     });
 
     $container->set('request_stack', function($c) {
-        return new $c['%request_stack.class%'];
+        return new $c['request_stack.class'];
     });
 
     $container->set('route_collection', function($c) {
-        return new $c['%route_collection.class%']();
+        return new $c['route_collection.class']();
     });
 
 
