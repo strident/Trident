2a8bfd33af3baee4504b5892dba76b8e7426e4e7
Elliot Wright <wright.elliot@gmail.com>
2014-05-24 22:42:17 +0100

    Fixed syntax error.

--- src/Trident/Module/FrameworkModule/config/services.php
+++ src/Trident/Module/FrameworkModule/config/services.php
@@ -14,45 +14,45 @@ return function($container) {
 
 
     // Services
-    $container->set('controller_resolver', function($c)) {
+    $container->set('controller_resolver', function($c) {
         return new $c['%controller_resolver.class%']($c);
-    };
+    });
 
-    $container->set('event_dispatcher', function($c)) {
+    $container->set('event_dispatcher', function($c) {
         return new $c['%event_dispatcher.class%']();
-    };
+    });
 
-    $container->set('http_kernel', function($c)) {
+    $container->set('http_kernel', function($c) {
         $dispatcher   = $c['event_dispatcher'];
         $resolver     = $c['controller_resolver'];
         $requestStack = $c['request_stack'];
 
         return new $c['%http_kernel.class%']($dispatcher, $resolver, $requestStack);
-    };
+    });
 
-    $container->set('listener.exception', function($c)) {
+    $container->set('listener.exception', function($c) {
         return new $c['%listener.exception.class%']($c['%exception_controller%']);
-    };
+    });
 
-    $container->set('listener.response', function($c)) {
+    $container->set('listener.response', function($c) {
         return new $c['%listener.response.class%']('UTF-8');
-    };
+    });
 
-    $container->set('listener.router', function($c)) {
+    $container->set('listener.router', function($c) {
         $routes  = $c['route_collection'];
         $context = new Symfony\Component\Routing\RequestContext();
         $matcher = new Symfony\Component\Routing\Matcher\UrlMatcher($routes, $context);
 
         return new $c['%listener.router.class%']($matcher);
-    };
+    });
 
-    $container->set('request_stack', function($c)) {
+    $container->set('request_stack', function($c) {
         return new $c['%request_stack.class%'];
-    };
+    });
 
-    $container->set('route_collection', function($c)) {
+    $container->set('route_collection', function($c) {
         return new $c['%route_collection.class%']();
-    };
+    });
 
 
     // Extensions
