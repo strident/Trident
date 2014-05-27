f53a7d042d383cc4657907c773d35c0f95c8f303
Elliot Wright <wright.elliot@gmail.com>
2014-05-27 14:37:46 +0100

    Added Twig serivce.

--- src/Trident/Module/TemplatingModule/module/config/services.php
+++ src/Trident/Module/TemplatingModule/module/config/services.php
@@ -2,11 +2,10 @@
 
 return function($container) {
     // Parameters
-    $container['templating.class'] = 'Trident\\Component\\HttpKernel\\Controller\\ControllerResolver';
 
 
     // Services
     $container->set('templating', function($c) {
-        return new $c['templating.class']();
+        return new Twig_Environment(null, []);
     });
 };
