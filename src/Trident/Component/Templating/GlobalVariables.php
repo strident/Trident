477e58cb2dc12dd403b9b103840a98c688c71ddb
Elliot Wright <wright.elliot@gmail.com>
2014-06-28 00:00:00 +0100

    Added safe mode to global variables and trident version segment on debug toolbar.

--- src/Trident/Component/Templating/GlobalVariables.php
+++ src/Trident/Component/Templating/GlobalVariables.php
@@ -55,9 +55,24 @@ class GlobalVariables
         return $this->container['kernel.debug'];
     }
 
+    /**
+     * Get application environment.
+     *
+     * @return mixed
+     */
     public function getEnvironment()
     {
         return $this->container['kernel.environment'];
+    }
+
+    /**
+     * Get application safe mode.
+     *
+     * @return boolean
+     */
+    public function getSafeMode()
+    {
+        return $this->container->get('kernel')->isSafeMode();
     }
 
     /**
