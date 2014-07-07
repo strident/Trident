25f8921605ca08f3cba5d67aa5dc5fc004d47feb
Elliot Wright <wright.elliot@gmail.com>
2014-07-07 20:55:39 +0100

    Lets start with some real version numbers now.

--- src/Trident/Component/HttpKernel/AbstractKernel.php
+++ src/Trident/Component/HttpKernel/AbstractKernel.php
@@ -49,11 +49,11 @@ abstract class AbstractKernel implements HttpKernelInterface
     protected $session;
     protected $startTime;
 
-    const VERSION         = '0.0.1-alpha';
-    const VERSION_ID      = '00101';
-    const MAJOR_VERSION   = '0';
+    const VERSION         = '1.0.0-alpha';
+    const VERSION_ID      = '10000';
+    const MAJOR_VERSION   = '1';
     const MINOR_VERSION   = '0';
-    const RELEASE_VERSION = '1';
+    const RELEASE_VERSION = '0';
     const EXTRA_VERSION   = 'alpha';
 
     /**
