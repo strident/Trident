57ea1534b358a839009b55fc0c03905a560ead80
Elliot Wright <wright.elliot@gmail.com>
2014-07-08 23:52:54 +0100

    Incremented minor version number to 1.0.2.

--- src/Trident/Component/HttpKernel/AbstractKernel.php
+++ src/Trident/Component/HttpKernel/AbstractKernel.php
@@ -49,11 +49,11 @@ abstract class AbstractKernel implements HttpKernelInterface
     protected $session;
     protected $startTime;
 
-    const VERSION         = '1.0.1-alpha';
-    const VERSION_ID      = '10001';
+    const VERSION         = '1.0.2-alpha';
+    const VERSION_ID      = '10002';
     const MAJOR_VERSION   = '1';
     const MINOR_VERSION   = '0';
-    const RELEASE_VERSION = '1';
+    const RELEASE_VERSION = '2';
     const EXTRA_VERSION   = 'alpha';
 
     /**
