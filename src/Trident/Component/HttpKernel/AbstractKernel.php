a112f0b75601b897c470a49d85791dc386c29952
Elliot Wright <wright.elliot@gmail.com>
2014-09-07 15:51:10 +0100

    Updated to new version of Aegis.

--- src/Trident/Component/HttpKernel/AbstractKernel.php
+++ src/Trident/Component/HttpKernel/AbstractKernel.php
@@ -51,12 +51,12 @@ abstract class AbstractKernel implements HttpKernelInterface
     protected $session;
     protected $startTime;
 
-    const VERSION         = '1.0.6-alpha4';
+    const VERSION         = '1.0.6-alpha5';
     const VERSION_ID      = '10006';
     const MAJOR_VERSION   = '1';
     const MINOR_VERSION   = '0';
     const RELEASE_VERSION = '6';
-    const EXTRA_VERSION   = 'alpha4';
+    const EXTRA_VERSION   = 'alpha5';
 
     /**
      * Constructor.
